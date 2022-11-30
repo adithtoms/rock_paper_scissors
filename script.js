012a8b9b8a37b3a3efbe17aa335557fb50033347
adithtoms <adithtoms@gmail.com>
2022-11-30 23:26:36 +0530

    initial stage complete

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 let compoint = 0;
 let yourpoint = 0;
+const outdiv=document.querySelector('#outdiv');
 
+const btn1 = document.querySelector('#btn1');
+const btn2 = document.querySelector('#btn2');
+
+const btn3 = document.querySelector('#btn3');
+
+const uyourscore=document.querySelector('.uyourscore');
+const ucomscore=document.querySelector('.ucomscore');
 
 const choices = ["rock", "paper", "scissor"]
 
@@ -9,53 +17,74 @@ function getComputerChoice() {
 
 };
 
-/*
-function playerSelection(){
-
-};
-
-function computerSelection(){
-
-};
-*/
-
-
-
-
 function playRound(playerSelection, computerSelection) {
-    console.log(playerSelection, computerSelection)
+    //console.log(playerSelection, computerSelection)
     if ((computerSelection === "paper" && playerSelection === "scissor") || (computerSelection === "rock" && playerSelection === "paper") || (computerSelection === "scissor" && playerSelection === "rock")) {
         yourpoint++;
-        return "you win ";
+        const p=document.createElement('p');
+        p.innerText="you win";
+        outdiv.appendChild(p);
+       
 
     } else if (computerSelection === playerSelection) {
-        return "tie"
+        const p=document.createElement('p');
+        p.innerText="tied";
+        outdiv.appendChild(p);
+        
     } else {
         compoint++;
-        return "you lose";
+        const p=document.createElement('p');
+        p.innerText="you lost";
+        outdiv.appendChild(p);
 
     };
 
 };
-
-
-
-
-function game() {
-    for (let i = 0; i < 5; i++) {
-        const computerSelection = getComputerChoice();
-        const playerSelection = prompt("enter ur choice");
-        console.log(playRound(playerSelection, computerSelection));
+const score=(yourpoint,compoint) => {
+    if(yourpoint === 5){
+        const h2=document.createElement('h2');
+        h2.innerText= `you won the game ${yourpoint} to ${compoint}`;
+        outdiv.append(h2);
     }
-    if (compoint > yourpoint) {
-        return "compuer wins game";
-    } else if (yourpoint > compoint) {
-        return "you win game";
-    } else {
-        return "game tied";
-    };
+    if(compoint === 5){
+        const h2=document.createElement('h2');
+        h2.innerText= ` you lost the game ${yourpoint} against ${compoint}`;
+        outdiv.append(h2);
+    }
+}
+ const updatescore=(yourpoint,compoint)=>{
+    uyourscore.innerText= `Your Point: ${yourpoint}`;
+    ucomscore.innerText=`Computer Point: ${compoint}`;
+ }
 
-};
 
-console.log(game());
 
+
+btn1.addEventListener('click', () => { 
+    const computerSelection = getComputerChoice();
+        const playerSelection = "rock"; 
+    playRound(playerSelection, computerSelection)
+    score(yourpoint,compoint);
+    updatescore(yourpoint,compoint);
+});
+   
+
+
+btn2.addEventListener('click',  () => { 
+    const computerSelection = getComputerChoice();
+        const playerSelection = "paper"; 
+    playRound(playerSelection, computerSelection)
+    score(yourpoint,compoint);
+    updatescore(yourpoint,compoint);
+});
+   
+
+btn3.addEventListener('click', () => { 
+    
+    const computerSelection = getComputerChoice();
+        const playerSelection = "scissor"; 
+    playRound(playerSelection, computerSelection)
+    score(yourpoint,compoint);
+    updatescore(yourpoint,compoint);
+});
+    
